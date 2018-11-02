e4b61c946d645b0e55d9471fcc02f7618f6b4b6a
Dev Patel <dhpatel15@gmail.com>
2018-11-02 13:34:50 -0400

    added teams components

--- proxy/server.js
+++ proxy/server.js
@@ -15,8 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/description', (req, res) => {
-  axios.get(`http://52.14.238.117:4000${req.url}`)
+  axios.get(`http://52.14.238.117${req.url}`)
     .then((results) => {
       res.send(results.data);
     })
@@ -26,16 +31,79 @@ app.get('/description', (req, res) => {
   });
 });
 
-
-app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+// Add STACY's API endpoints
+app.get('/ratings', (req, res) => {
+  axios.get(`http://18.218.27.164${req.url}`)
+    .then((results) => {
+      // console.log(results.data);
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send();
+    });
 });
 
+app.get('/reviews', (req, res) => {
+  axios.get(`http://18.218.27.164${req.url}`)
+    .then((results) => {
+      // console.log(results.data);
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send();
+    });
+});
+
+app.get('/search', (req, res) => {
+  axios.get(`http://18.218.27.164${req.url}`)
+    .then((results) => {
+      // console.log(results.data);
+      res.send(results.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send();
+    });
+});
+
+// Add Louis's API endpoints
+
+app.get('/listing', (req, res)=>{
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+});
+
+
+// Add DAVID's API endpoints
+app.get('/listingdata', (req, res) => {
+  let requestId = req.query.id;
+  requestId = requestId.slice(-3) * 1;
+  axios.get(`http://3.16.89.66/listingdata?id=${requestId}`)
+  .then((results) => res.send(results.data))
+  .catch((err) => console.error(err));
+})
+
+app.get('/neighborhooddata', (req, res) => {
+  let requestId = req.query.id;
+  requestId = requestId.slice(-3) * 1;
+  axios.get(`http://3.16.89.66/neighborhooddata?id=${requestId}`)
+  .then((results) => res.send(results.data))
+  .catch((err) => console.error(err));
+})
+
+app.get('/landmarkdata', (req, res) => {
+  let lat = req.query.listingLat;
+  let long = req.query.listingLong;
+  axios.get(`http://3.16.89.66/landmarkdata?listingLat=${lat}&listingLong=${long}`)
+  .then((results) => res.send(results.data))
+  .catch((err) => console.error(err));
+})
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
 
 
 app.listen(port, function() {
